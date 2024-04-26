b13d3fe2c973dc165d91dafa67f968b930e11155
Jose Paredes <joseaplwork@gmail.com>
2024-04-26 19:37:47 +0200

    Update Netlify configuration and remove unused API endpoint for cocktails

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-/* eslint-disable */
 const requestProxy = require('express-request-proxy');
 const express = require('express');
-const cors = require('cors');
+const path = require('path');
 const app = express();
+const router = express.Router();
 const port = 3005;
 
-app.use(cors());
-app.use(express.static('build'));
-app.all('/api/', requestProxy({ url: 'http://www.thecocktaildb.com/api/json/v1/1/search.php' }));
+router.get('/api/', requestProxy({ url: 'http://www.thecocktaildb.com/api/json/v1/1/search.php' }));
+app.use(express.static(path.join(__dirname, '../build')));
+app.use(router);
 app.listen(port, () => console.log(`Server listening on port http://localhost:${port} !👂🚀`));
-/* eslint-enable */
